dba59b046cf5c25e15264aa0bdab40ea53375152
preston <broberts9@gmail.com>
2024-07-10 23:05:32 -0700

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
+let target;
+if (process.env.NODE_ENV == 'production') {
+  target = "node-koa-app.azurewebsites.net"
+}
+else {
+	target = 'http://localhost:5501'
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
 	server: {
     proxy: {
       "/test": {
-        target: "node-koa-app.azurewebsites.net",
+        target,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
